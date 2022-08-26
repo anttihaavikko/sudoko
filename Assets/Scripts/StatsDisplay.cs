e0ad38ea4fae783c6a6891d58ced6c63e1ceea64
Antti Haavikko <antti.haavikko@gmail.com>
2022-08-26 17:10:21 +0300

    Add stats text shadow

--- Assets/Scripts/StatsDisplay.cs
+++ Assets/Scripts/StatsDisplay.cs
@@ -1,13 +1,14 @@
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
 public class StatsDisplay : MonoBehaviour
 {
-    [SerializeField] private TMP_Text attack, defense;
+    [SerializeField] private List<TMP_Text> attack, defense;
     
     public void Set(Stats stats)
     {
-        attack.text = stats.attack.ToString();
-        defense.text = stats.defence.ToString();
+        attack.ForEach(a => a.text = stats.attack.ToString());
+        defense.ForEach(d => d.text = stats.defence.ToString());
     }
 }
