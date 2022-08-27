11efad02aa57a2988f8dfa787ed230be69859fbe
Antti Haavikko <antti.haavikko@gmail.com>
2022-08-27 22:30:57 +0300

    Add menu button

--- Assets/Scripts/Restarter.cs
+++ Assets/Scripts/Restarter.cs
@@ -7,9 +7,20 @@ public class Restarter : MonoBehaviour
 {
     public void Restart()
     {
+        Clear();
+        SceneChanger.Instance.ChangeScene("Map");
+    }
+
+    public void Back()
+    {
+        Clear();
+        SceneChanger.Instance.ChangeScene("Start");
+    }
+
+    private static void Clear()
+    {
         AudioManager.Instance.TargetPitch = 1f;
         MapState.Instance.Clear();
         StateManager.Instance.Clear();
-        SceneChanger.Instance.ChangeScene("Map");
     }
 }
